31f47a442e466cea0cc8fc445735acbdeb25787e
l l <dopodraul@gmail.com>
2024-07-26 10:16:20 +0800

    Todolist 增刪改

--- rn-todolist/App.tsx
+++ rn-todolist/App.tsx
@@ -1,19 +1,83 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Text, View, SafeAreaView, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 // You can import supported modules from npm
 import { Card } from 'react-native-paper';
+import { useState, useEffect } from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // or any files within the Snack
 import TodolistComponent from './components/TodolistComponent';
 
+const TodolistCard = (props: {
+  todoList: { id: number, isDone: boolean; text: string; }[],
+  remove: (id: number) => void
+}) => {
+  const [todoList, setTodoList] = useState(props.todoList);
+
+  useEffect(() => {
+    setTodoList(props.todoList);
+  }, [props]);
+
+  if (todoList[0]) {
+    return (
+      <View>{
+        todoList.map((hash) => (
+          <TodolistComponent
+            id={hash.id}
+            isDone={hash.isDone}
+            text={hash.text}
+            remove={props.remove} />
+        ))
+      }</View>
+    );
+  }
+
+  return (<View><Text>尚無待辦項目，請由上列選單添加</Text></View>);
+}
+
 export default function App() {
+  const [id, setId] = useState(1);
+  const [text, setText] = useState('');
+  const [todoList, setTodoList] = useState<any[]>([]);
+  const [addColor, setAddColor] = useState('');
+
+  const addTodo = () => {
+    if (text) {
+      setTodoList([...todoList, { id, text, isDone: false }]);
+      setId(id + 1);
+      setText('');
+    }
+  }
+
+  const removeTodo = (id: number) => {
+    setTodoList( todoList.filter(hash => {
+      return id !== hash.id;
+    }));
+  }
+
+  useEffect(() => {
+    setAddColor(text ? 'black' : '#fffaf0');
+  }, [text]);
+
   return (
     <SafeAreaView style={styles.container}>
       <Card style={styles.card}>
-        <Text></Text>
+        <View style={styles.menu}>
+          <View style={styles.menuInput}>
+            <TextInput
+              value={text}
+              onChangeText={setText}
+              placeholder="請輸入待辦項目" />
+          </View>
+          <TouchableOpacity style={styles.menuAdd} onPress={addTodo}>
+            <View>
+              <Icon name="plus" color={addColor} />
+            </View>
+          </TouchableOpacity>
+        </View>
       </Card>
       <Card style={styles.card}>
-        <TodolistComponent isDone={true} text="Content" />
+        <TodolistCard todoList={todoList} remove={removeTodo} />
       </Card>
     </SafeAreaView>
   );
@@ -22,7 +86,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     backgroundColor: '#ecf0f1',
     padding: 8,
     marginTop: 32,
@@ -31,7 +94,21 @@ const styles = StyleSheet.create({
     marginRight: 16
   },
   card: {
-    padding: 8,
-    marginBottom: 8
+    padding: 16,
+    margin: 16
+  },
+  menu: {
+    flexDirection: 'row',
+  },
+  menuInput: {
+    flex: 5,
+    borderWidth: 1,
+    padding: 8
+  },
+  menuAdd: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    marginRight: 24
   }
 });
--- rn-todolist/components/TodolistComponent.tsx
+++ rn-todolist/components/TodolistComponent.tsx
@@ -1,35 +1,64 @@
-import { useState, useEffect, PropsWithChildren } from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-type TodolistProps = PropsWithChildren<{
+export default function TodolistComponent( props: {
+  id: number;
   isDone: boolean;
   text: string;
-}>;
-
-export default function TodolistComponent( props: TodolistProps ) {
+  remove: (id: number) => void
+}) {
   const [isDone, setIsDone] = useState(props.isDone);
   const [doneIcon, setDoneIcon] = useState('');
-  const [text, setText] = useState(props.text);
+  const [textShow, setTextShow] = useState('');
+  const [textSave, setTextSave] = useState('');
+
+  const checkDone = () => {
+    setIsDone(!isDone);
+  }
+
+  const onChangeText = (value: string) => {
+    setTextShow(value);
+  }
+
+  const onBlur = () => {
+    if (textShow) {
+      setTextSave(textShow);
+    } else {
+      setTextShow(textSave);
+    }
+  }
+
+  const removeTodo = () => {
+    props.remove(props.id);
+  }
 
   useEffect(() => {
     setDoneIcon(isDone ? 'check-square-o' : 'square-o');
   }, [isDone]);
 
+  useEffect(() => {
+    setTextShow(props.text);
+    setTextSave(props.text);
+  }, [props]);
+
   return (
     <TouchableOpacity>
       <View style={styles.item}>
-        <TouchableOpacity style={styles.check}>
+        <TouchableOpacity style={styles.check} onPress={checkDone}>
           <View>
             <Icon name={doneIcon} />
           </View>
         </TouchableOpacity>
         <TouchableOpacity style={styles.text}>
           <View>
-            <Text>{text}</Text>
+            <TextInput
+              value={textShow}
+              onChangeText={onChangeText}
+              onBlur={onBlur} />
           </View>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.remove}>
+        <TouchableOpacity style={styles.remove} onPress={removeTodo}>
           <View>
             <Icon name="remove" />
           </View>
@@ -51,7 +80,8 @@ const styles = StyleSheet.create({
   },
   check: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginLeft: 16
   },
   text: {
     flex: 10
@@ -59,6 +89,7 @@ const styles = StyleSheet.create({
   remove: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'flex-end'
+    alignItems: 'flex-end',
+    marginRight: 16
   }
 });
