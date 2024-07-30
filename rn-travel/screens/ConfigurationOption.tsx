708fd5fb298e586d5da421dd385e344cc0688b7a
l l <dopodraul@gmail.com>
2024-07-30 12:58:34 +0800

    旅遊APP 設定頁

--- rn-travel/screens/ConfigurationOption.tsx
+++ rn-travel/screens/ConfigurationOption.tsx
@@ -1,17 +1,48 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
-export default function ConfigurationOption() {
+const listData = [
+  { title: 'Language', value: 'English' },
+  { title: 'Color', value: 'Light' },
+  { title: 'App Version', value: '1.0.0' },
+  { title: 'Last Updated', value: 'July 14, 2024' },
+];
+
+const renderItem = ({ item }: any) => (
+  <View style={styles.item}>
+    <Text style={styles.itemName}>{item.title}</Text>
+    <Text style={styles.itemValue}>{item.value}</Text>
+  </View>
+);
+
+const ConfigurationOption = () => {
   return (
     <View style={styles.container}>
-      <Text>設定</Text>
+      <FlatList
+        data={listData}
+        renderItem={renderItem}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    padding: 20,
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 15
+  },
+  itemName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  itemValue: {
+    fontSize: 16
+  },
 });
+
+export default ConfigurationOption;
