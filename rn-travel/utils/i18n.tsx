d31e692170e79fdbad4ea030783b28fdd26468fc
l l <dopodraul@gmail.com>
2024-07-31 08:47:33 +0800

    旅遊APP 首頁、設定頁語言包

--- rn-travel/utils/i18n.tsx
+++ rn-travel/utils/i18n.tsx
@@ -3,6 +3,10 @@ import { initReactI18next } from 'react-i18next';
 
 import indexEn from '../json/indexEn.json';
 import configurationOptionEn from '../json/configurationOptionEn.json';
+import indexZhTw from '../json/indexZhTw.json';
+import configurationOptionZhTw from '../json/configurationOptionZhTw.json';
+import indexJa from '../json/indexJa.json';
+import configurationOptionJa from '../json/configurationOptionJa.json';
 
 i18n.use(initReactI18next).init({
   lng: 'en',
@@ -16,6 +20,16 @@ i18n.use(initReactI18next).init({
     en: {
       index: indexEn,
       configurationOption: configurationOptionEn
+    },
+
+    ja: {
+      index: indexJa,
+      configurationOption: configurationOptionJa
+    },
+
+    'zh-TW': {
+      index: indexZhTw,
+      configurationOption: configurationOptionZhTw
     }
   }
 });
