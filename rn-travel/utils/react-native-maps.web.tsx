21d9dec0fc85ab1fa7a2cda4071577a06719fedf
l l <dopodraul@gmail.com>
2024-08-19 07:59:38 +0800

    旅遊APP Web版map

--- rn-travel/utils/react-native-maps.web.tsx
+++ rn-travel/utils/react-native-maps.web.tsx
@@ -1,12 +1,77 @@
-import { View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { ReactNode } from 'react';
+import L, { LatLngExpression } from 'leaflet';
 
-const MapView = ({ children }: { children: ReactNode }) => {
-  return (<View>{children}</View>);
+import {
+  MapContainer,
+  TileLayer,
+  Popup,
+  AttributionControl,
+  Marker as LeafletMarker
+} from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+type LatLng = {
+  latitude: number;
+  longitude: number;
+};
+
+const MapView = ({ children, region }: {
+  children: ReactNode;
+  region: LatLng;
+}) => {
+  const center: LatLngExpression = [region.latitude, region.longitude];
+  const prefix = `<a href='https://www.google.com/maps?q=${region.latitude},${region.longitude}' target="_blank">Google Maps</a>`;
+
+  return (
+    <MapContainer
+      center={center}
+      style={styles.map}
+      zoom={16}
+      attributionControl={false}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+      <AttributionControl
+        position="bottomright"
+        prefix={prefix}
+      />
+      {children}
+    </MapContainer>
+  );
 }
 
-const Marker = () => {
-  return (<View />);
+const Marker = ({
+  title,
+  description,
+  coordinate
+}: {
+  title: string;
+  description: string;
+  coordinate: LatLng;
+}) => {
+  const position: LatLngExpression = [coordinate.latitude, coordinate.longitude];
+  const icon = new L.Icon({ iconUrl: 'https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png' });
+
+  return (
+    <LeafletMarker
+      position={position}
+      icon={icon}
+    >
+      <Popup>
+        {title} <br />
+        {description}
+      </Popup>
+    </LeafletMarker>
+  );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    width: '95%',
+    height: 360
+  }
+});
 
 export { MapView, Marker };
