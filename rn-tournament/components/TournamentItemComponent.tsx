9a4aa14ab1405e567b55885341db1126ba6290c6
l l <dopodraul@gmail.com>
2024-09-27 08:21:00 +0800

    賽程APP 每個賽程改成component

---
+++ rn-tournament/components/TournamentItemComponent.tsx
@@ -0,0 +1,52 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native'
+
+import { useContext } from 'react'
+import Icon from 'react-native-vector-icons/Ionicons'
+import { screenEnum, AppContext } from '../utils/common'
+
+export default function TournamentItemComponent({ item, navigation }) {
+  const { getStyle } = useContext(AppContext)
+  const stylesColor = getStyle()
+  const stylesBorder = { borderColor: stylesColor.color }
+
+  const pressEdit = () => {
+    navigation.navigate(screenEnum.tournamentEdit, { id: item.id })
+  }
+
+  return (
+    <View style={[styles.component, stylesBorder]}>
+      <View style={styles.name}>
+        <Text style={stylesColor}>{item.name}</Text>
+      </View>
+      <View style={styles.icon}>
+        <TouchableOpacity onPress={pressEdit}>
+          <Icon name="pencil" size={24} color={stylesColor.color} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  component: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 8,
+    marginBottom: 8,
+    borderWidth: 1,
+    borderRadius: 16
+  },
+
+  name: {
+    flex: 1
+  },
+
+  icon: {
+    flexDirection: 'row'
+  }
+})
--- rn-tournament/screens/HomeScreen.tsx
+++ rn-tournament/screens/HomeScreen.tsx
@@ -2,39 +2,19 @@ import {
   View,
   Text,
   FlatList,
-  TouchableOpacity,
   StyleSheet
 } from 'react-native'
 
 import { useContext } from 'react'
-import Icon from 'react-native-vector-icons/Ionicons'
-import { screenEnum, AppContext } from '../utils/common'
+import { AppContext } from '../utils/common'
+import TournamentItemComponent from '../components/TournamentItemComponent'
 
 export default function HomeScreen({ navigation }) {
   const { tournamentList, getStyle } = useContext(AppContext)
   const stylesColor = getStyle()
-  const stylesBorder = { borderColor: stylesColor.color }
 
   const renderItem = ({ item }) => {
-    const pressEdit = () => {
-      navigation.navigate(screenEnum.tournamentEdit, { id: item.id })
-    }
-
-    return (
-      <View style={[styles.item, stylesBorder]}>
-        <View style={styles.itemName}>
-          <Text style={stylesColor}>{item.name}</Text>
-        </View>
-        <View style={styles.itemIcon}>
-          <TouchableOpacity style={styles.itemIconEdit} onPress={pressEdit}>
-            <Icon name="pencil" size={24} color={stylesColor.color} />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Icon name="swap-vertical" size={24} color={stylesColor.color} />
-          </TouchableOpacity>
-        </View>
-      </View>
-    )
+    return (<TournamentItemComponent item={item} navigation={navigation} />)
   }
 
   const content = tournamentList[0] ?
@@ -55,26 +35,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32
-  },
-
-  item: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 8,
-    marginBottom: 8,
-    borderWidth: 1,
-    borderRadius: 16
-  },
-
-  itemName: {
-    flex: 1
-  },
-
-  itemIcon: {
-    flexDirection: 'row'
-  },
-
-  itemIconEdit: {
-    marginRight: 8
   }
 })
